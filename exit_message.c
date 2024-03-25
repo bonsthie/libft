241846da2af515d096c2fed8c6db4072536e14b6
Barnabe Bonnet <babonnet@3E3.42angouleme.fr>
2024-03-25 20:02:47 +0100

    yes

--- exit_message.c
+++ exit_message.c
@@ -6,7 +6,7 @@
 /*   By: babonnet <babonnet@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 16:04:56 by babonnet          #+#    #+#             */
-/*   Updated: 2024/03/19 16:06:02 by babonnet         ###   ########.fr       */
+/*   Updated: 2024/03/22 16:40:12 by babonnet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 void	exit_message(int error_value, char *message)
 {
 	ft_putstr_fd(message, 2);
+
 	exit(error_value);
 }
